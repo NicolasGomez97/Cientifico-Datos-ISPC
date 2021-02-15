f44273ea9db6720b2f0bb4357d8fc1c9b5daf04b
Your Name <you@example.com>
2021-02-15 18:30:52 -0300

    Prog 15/02/21

--- Analisis Matematico/Ejemplos/Codigo.py
+++
@@ -1,6 +0,0 @@
-from sklearn import linear_model
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import numpy as np
-import csv
-
--- Analisis Matematico/Ejemplos/CursoDePandas/Pandas-Curso2.py
+++ Analisis Matematico/Ejemplos/CursoDePandas/Pandas-Curso2.py
@@ -1,6 +1,19 @@
-import seaborn as sns
 import pandas as pd
-import sys
-import csv
-datos = pd.read_csv("C:/Users/Nicolas/Documents/Cientifico-Datos-ISPC/Programacion I\Datasets\vgsales.csv\vgsales.csv")
-print(datos)
+import numpy as np
+datos = pd.read_csv("C:/Users/Nicolas/Documents/Cientifico-Datos-ISPC/Programacion-I/Datasets/vgsales.csv/vgsales.csv")
+print(datos.info())
+print(datos.head())
+
+nuevo = pd.DataFrame(datos)
+print(nuevo)
+nuevo = nuevo.replace(np.nan,"0")
+print("****Impresión sin NaN****")
+print(nuevo.info())
+print("\n"*5)
+print("****Estadísticas sin NaN****")
+print(nuevo.describe())
+print("\n"*5)
+print("****Estadísticas solo numeros****")
+print(nuevo.describe(include=[np.number]))
+print("\n"*5)
+
